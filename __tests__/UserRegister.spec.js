4f3ad58e6f57a21610dfd53c8675c69aebbdd1de
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-02-14 16:10:21 +0600

    body and status test cases added

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
 
-it('should return 200 when signup request is valid', () => {
+it('should return 200 when signup request is valid', (done) => {
   request(app)
     .post('/api/1.0/users')
     .send({
@@ -9,5 +9,22 @@ it('should return 200 when signup request is valid', () => {
       email: 'user1@gmail.com',
       password: 'p4ssword',
     })
-    .expect(200);
+    .then((response) => {
+      expect(response.status).toBe(200);
+      done();
+    });
+});
+
+it('should return Success Message when signup request is valid', (done) => {
+  request(app)
+    .post('/api/1.0/users')
+    .send({
+      username: 'user1',
+      email: 'user1@gmail.com',
+      password: 'p4ssword',
+    })
+    .then((response) => {
+      expect(response.body.message).toBe('User Created');
+      done();
+    });
 });
--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.post('/api/1.0/users', (req, res) => {
+  return res.status(200).send({
+    message: 'User Created',
+  });
+});
+
 module.exports = app;
