5510d94256f6fcf459e79304dd8896a85dab4900
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-02-14 16:45:22 +0600

    Wrapped test cases  with describe as a heading for the tests

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,30 +1,32 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../src/app');
 
-it('should return 200 when signup request is valid', (done) => {
-  request(app)
-    .post('/api/1.0/users')
-    .send({
-      username: 'user1',
-      email: 'user1@gmail.com',
-      password: 'p4ssword',
-    })
-    .then((response) => {
-      expect(response.status).toBe(200);
-      done();
-    });
-});
+describe('User Registration', () => {
+  it('should return 200 when signup request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'p4ssword',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
 
-it('should return Success Message when signup request is valid', (done) => {
-  request(app)
-    .post('/api/1.0/users')
-    .send({
-      username: 'user1',
-      email: 'user1@gmail.com',
-      password: 'p4ssword',
-    })
-    .then((response) => {
-      expect(response.body.message).toBe('User Created');
-      done();
-    });
+  it('should return Success Message when signup request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'p4ssword',
+      })
+      .then((response) => {
+        expect(response.body.message).toBe('User Created');
+        done();
+      });
+  });
 });
