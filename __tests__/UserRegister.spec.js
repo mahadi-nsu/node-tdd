4cd0214b4789841e126b379d2b8ff30251236908
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-02-26 21:47:59 +0600

    refactor : user registration test case refactired

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -12,86 +12,59 @@ beforeEach(() => {
   return User.destroy({ truncate: true });
 });
 
+const postValidUsers = () => {
+  return request(app).post('/api/1.0/users').send({
+    username: 'user1',
+    email: 'user1@gmail.com',
+    password: 'p4ssword',
+  });
+};
+
 describe('User Registration', () => {
   it('should return 200 when signup request is valid', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'p4ssword',
-      })
-      .then((response) => {
-        // console.log(response);
-        expect(response.status).toBe(200);
-        done();
-      });
+    postValidUsers().then((response) => {
+      // console.log(response);
+      expect(response.status).toBe(200);
+      done();
+    });
   });
 
   it('should return Success Message when signup request is valid', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'p4ssword',
-      })
-      .then((response) => {
-        expect(response.body.message).toBe('User Created');
-        done();
-      });
+    postValidUsers().then((response) => {
+      expect(response.body.message).toBe('User Created');
+      done();
+    });
   });
 
   it('should save the user to the database', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'p4ssword',
-      })
-      .then((response) => {
-        logger.warn(response);
+    postValidUsers().then((response) => {
+      logger.warn(response);
 
-        User.findAll().then((userList) => {
-          expect(userList.length).toBe(1);
-          done();
-        });
+      User.findAll().then((userList) => {
+        expect(userList.length).toBe(1);
+        done();
       });
+    });
   });
 
   it('should save username and email', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'p4ssword',
-      })
-      .then(() => {
-        User.findAll().then((userList) => {
-          // const savedUser = userList[0];
-          expect(userList[0].username).toBe('user1');
-          expect(userList[0].email).toBe('user1@gmail.com');
-          done();
-        });
+    postValidUsers().then(() => {
+      User.findAll().then((userList) => {
+        // const savedUser = userList[0];
+        expect(userList[0].username).toBe('user1');
+        expect(userList[0].email).toBe('user1@gmail.com');
+        done();
       });
+    });
   });
 
   it('should hash password', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'p4ssword',
-      })
-      .then(() => {
-        User.findAll().then((userList) => {
-          console.log('passowrd we get', userList[0].password);
-          expect(userList[0].password).not.toBe('p4ssword');
-          done();
-        });
+    postValidUsers().then(() => {
+      User.findAll().then((userList) => {
+        console.log('passowrd we get', userList[0].password);
+        expect(userList[0].password).not.toBe('p4ssword');
+        done();
       });
+    });
   });
 });
