8a58b545f5a7cce2396432233910fe0e53168c5d
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-02-26 19:32:48 +0600

    improvement : more simpler approach to form new user object

--- src/app.js
+++ src/app.js
@@ -8,11 +8,16 @@ app.use(express.json());
 
 app.post('/api/1.0/users', (req, res) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = {
-      username: req.body.username,
-      email: req.body.email,
-      password: hash,
-    };
+    // approach - 1
+
+    // const user = {
+    //   username: req.body.username,
+    //   email: req.body.email,
+    //   password: hash,
+    // };
+
+    // approach - 2
+    const user = { ...req.body, password: hash };
 
     User.create(user).then((response) => {
       return res.send({ message: 'User Created', response: response });
