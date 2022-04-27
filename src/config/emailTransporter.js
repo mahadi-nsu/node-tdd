b11695efee0549f37c13cec161f62c32982fe03e
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-04-27 23:10:30 +0600

    email send not working

--- src/config/emailTransporter.js
+++ src/config/emailTransporter.js
@@ -1,5 +1,11 @@
-const nodemailerStub = require('nodemailer-stub');
+// const nodemailerStub = require('nodemailer-stub');
 const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport(nodemailerStub.stubTransport);
+const transporter = nodemailer.createTransport({
+  host: 'localhost',
+  port: '8587',
+  tls: {
+    rejectUnauthorized: true,
+  },
+});
 
 module.exports = transporter;
