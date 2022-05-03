48a765643acf13fc13afe7ea6dcfba0800ee45e5
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-05-03 23:07:24 +0600

    gmail service email sent working

--- src/config/emailTransporter.js
+++ src/config/emailTransporter.js
@@ -1,10 +1,14 @@
 // const nodemailerStub = require('nodemailer-stub');
 const nodemailer = require('nodemailer');
+
 const transporter = nodemailer.createTransport({
-  host: 'localhost',
-  port: '8587',
-  tls: {
-    rejectUnauthorized: true,
+  host: "smtp.gmail.com",
+  service: "gmail",
+  port: 465,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "virginawalker45@gmail.com", // ethereal user
+    pass: "wal$#@!~365", // ethereal password
   },
 });
 
